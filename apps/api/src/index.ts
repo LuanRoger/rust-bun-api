1c055eb5c750cd71139f4856d79bb85ad9f0f759
ROG <luan.roger.2003@gmail.com>
2026-01-11 19:31:30 -0300

    feat: use core in api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,13 @@
+import { plus100 } from "@repo/core";
 import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/:value", (c) => {
+  const value = c.req.param("value");
+  const pasedValue = Number.parseInt(value, 10);
+
+  return c.json({ message: "Hello, World!", plus100: plus100(pasedValue) });
 });
 
 export default app;
